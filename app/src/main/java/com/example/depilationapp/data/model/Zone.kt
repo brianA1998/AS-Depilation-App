8c7a60b7e31308d7b02493082f3309fa9b5bc394
brianA1998 <brian.alaniz@hotmail.com>
2023-01-25 22:40:04 -0300

    Models develop

---
+++ app/src/main/java/com/example/depilationapp/data/model/Zone.kt
@@ -0,0 +1,15 @@
+package com.example.depilationapp.data.model
+
+enum class Zone(zone: String) {
+    LEGS("PIERNA/S"),
+    ARM("BRAZO/S"),
+    DUG("CAVADO"),
+    DOWN("BOZO"),
+    MUSTACHE("BIGOTE"),
+    SIDEBURN("PATILLA"),
+    BACK("ESPALDA"),
+    ARMPITS("AXILAS"),
+    CHEST("PECHO"),
+}
+
+
