24b06fe5a61d0e945dad2ceabaa3588f00ea94f2
brianA1998 <brian.alaniz@hotmail.com>
2023-03-18 18:26:10 -0300

    UI de la lista modificada

--- app/src/main/java/com/example/depilationapp/presentation/clients/components/ClientsContent.kt
+++ app/src/main/java/com/example/depilationapp/presentation/clients/components/ClientsContent.kt
@@ -1,13 +1,22 @@
 package com.example.depilationapp.presentation.clients.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import com.example.depilationapp.R
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
@@ -30,20 +39,35 @@ fun ClientsContent(clients: List<Client>, navController: NavHostController) {
 
 @Composable
 fun ClientCard(client: Client, onClick: () -> Unit) {
-    
 
-
-
-
-    Text(
-        text = client.name,
-        color = Color.Black,
-        fontSize = 24.sp,
+    Card(
         modifier = Modifier
             .fillMaxWidth()
             .clickable { onClick() }
-            .padding(16.dp)
-    )
+            .padding(vertical = 4.dp),
+        shape = RoundedCornerShape(8.dp),
+        elevation = 4.dp
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+            Column {
+                Text(
+                    text = client.name,
+                    style = MaterialTheme.typography.subtitle1.copy(fontWeight = FontWeight.Bold)
+                )
+                Text(
+                    text = "De : ${client.province}",
+                    style = MaterialTheme.typography.body2
+                )
+
+            }
+        }
+    }
 }
 
 @Composable
