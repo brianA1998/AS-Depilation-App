f0aff22acdf7c23e328931187aee4c591ad918ea
brianA1998 <brian.alaniz@hotmail.com>
2023-04-05 21:46:52 -0300

    Mejorando la UI de detalle de cliente

--- app/src/main/java/com/example/depilationapp/presentation/clients/components/DetailScreen.kt
+++ app/src/main/java/com/example/depilationapp/presentation/clients/components/DetailScreen.kt
@@ -1,18 +1,87 @@
 package com.example.depilationapp.presentation.clients.components
 
-import android.util.Log
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Text
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.depilationapp.data.model.Client
 
+
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun DetailScreen(client: Client) {
-    Column(modifier = Modifier.padding(16.dp)) {
-        Text(text = "Nombre del cliente: ${client.name}")
-        Log.i("detalle","$client")
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Detalle del Cliente") },
+                backgroundColor = MaterialTheme.colors.primary
+            )
+        },
+        content = {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Card(
+                    shape = RoundedCornerShape(12.dp),
+                    backgroundColor = MaterialTheme.colors.primary,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text(
+                        text = client.name,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 24.sp,
+                        color = Color.White,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp)
+                    )
+                }
+                Spacer(modifier = Modifier.padding(16.dp))
+                DetailItem(title = "Apellido", value = client.surname)
+                DetailItem(title = "Documento", value = client.document?.toString() ?: "")
+                DetailItem(title = "Provincia", value = client.province ?: "")
+                DetailItem(
+                    title = "Teléfono personal",
+                    value = client.numberPhonePersonal.toString()
+                )
+                DetailItem(title = "Teléfono adicional", value = client.numberPhoneOther.toString())
+                DetailItem(title = "Estado", value = if (client.state) "Activo" else "Inactivo")
+                DetailItem(title = "Zona de Retoque", value = client.listZoneRetoque ?: "")
+                DetailItem(title = "Zona", value = client.Zone ?: "")
+                DetailItem(title = "Observaciones", value = client.observation)
+            }
+        }
+    )
+}
+
+@Composable
+fun DetailItem(title: String, value: String) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 4.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = title,
+            fontWeight = FontWeight.Bold,
+            fontSize = 18.sp,
+            modifier = Modifier.width(180.dp)
+        )
+        Spacer(modifier = Modifier.padding(8.dp))
+        Text(
+            text = value,
+            fontSize = 18.sp
+        )
     }
 }
