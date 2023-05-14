01d3145a9e1fc75798cab390fd01a60db33f257b
brianA1998 <brian.alaniz@hotmail.com>
2023-05-14 19:15:06 -0300

    Validando caracteres nuemricos

--- app/src/main/java/com/example/depilationapp/presentation/clients/components/AddClientScreen.kt
+++ app/src/main/java/com/example/depilationapp/presentation/clients/components/AddClientScreen.kt
@@ -3,6 +3,7 @@ package com.example.depilationapp.presentation.clients.components
 import android.annotation.SuppressLint
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
@@ -11,6 +12,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
@@ -53,14 +55,6 @@ fun AddClientScreen(navController: NavHostController, useCases: UseCases) {
                 .padding(16.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(
-                text = "Agregar un nuevo cliente",
-                fontWeight = FontWeight.Bold,
-                fontSize = 24.sp,
-                modifier = Modifier.padding(bottom = 16.dp)
-            )
-
-
             OutlinedTextField(
                 value = name,
                 onValueChange = setName,
@@ -81,11 +75,16 @@ fun AddClientScreen(navController: NavHostController, useCases: UseCases) {
 
             OutlinedTextField(
                 value = document,
-                onValueChange = setDocument,
+                onValueChange = { newInput ->
+                    if (newInput.all { it.isDigit() }) {
+                        setDocument(newInput)
+                    }
+                },
                 label = { Text("Documento") },
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(bottom = 8.dp)
+                    .padding(bottom = 8.dp),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
             )
 
             OutlinedTextField(
